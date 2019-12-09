708391104d021b152bc15275243857e2e846d73c
luyuanpeng <karexy@qq.com>
2019-12-09 21:07:39 +0800

    try to draw something

--- SharpRender/SharpRender/SharpForm.cs
+++ SharpRender/SharpRender/SharpForm.cs
@@ -17,5 +17,16 @@ namespace SharpRender
             InitializeComponent();
         }
 
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Bitmap myBitmap = new Bitmap(700, 700);
+            for (var x = 0; x < 100; x++)
+                for (var y = 0; y < 100; y++)
+                {
+                    myBitmap.SetPixel(x, y, Color.Red);
+                }
+            e.Graphics.DrawImage(myBitmap, 0, 0);
+        }
     }
 }
